8f4321181d02dcc04b803982c75bcdcd361428cd
xiaojuanliu826 <xiaojuanliu@academic.rrc.ca>
2026-03-25 11:34:57 -0500

    Define Many-to-Many relationship and validations in Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  # a category has multiple products
+  has_many :product_categories
+
+  has_many :products, through: :product_categories
+
+  # ensure category name is present
+  validates :name, presence: true
 end
